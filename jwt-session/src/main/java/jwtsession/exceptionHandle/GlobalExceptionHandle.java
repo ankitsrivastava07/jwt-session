6c6be4a3e18d3b8a6a453ae7da389ec447ad6521
qtpl0036 <ravish.rathi@qtsolv.com>
2021-06-13 16:17:55 +0530

    change

--- jwt-session/src/main/java/jwtsession/exceptionHandle/GlobalExceptionHandle.java
+++ jwt-session/src/main/java/jwtsession/exceptionHandle/GlobalExceptionHandle.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.BooleanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +11,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import io.jsonwebtoken.ClaimJwtException;
 import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.MalformedJwtException;
 
 @ControllerAdvice
@@ -24,16 +21,14 @@ public class GlobalExceptionHandle {
 	private HttpServletRequest path;
 
 	@ExceptionHandler(ExpiredJwtException.class)
-	public ResponseEntity<?> tokenExpiredException(ExpiredJwtException exception) {
+	public ResponseEntity<?> tokenException(ExpiredJwtException exception) {
 
 		TokenStatus tokenStatus = new TokenStatus();
-
 		tokenStatus.setStatus(Boolean.FALSE);
 		tokenStatus.setMessage("Your session has been expired.Please login again");
-
 		return new ResponseEntity<>(tokenStatus, HttpStatus.OK);
 	}
-
+	
 	@ExceptionHandler(MalformedJwtException.class)
 	public ResponseEntity<?> tokenException(MalformedJwtException exception) {
 
