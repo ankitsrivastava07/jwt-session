cf55e56ee9e369d0de2eb643e039fc3f662fb68d
qtpl0036 <ravish.rathi@qtsolv.com>
2021-06-18 00:52:41 +0530

    cange

--- jwt-session/src/main/java/jwtsession/service/UserServiceProxy.java
+++ jwt-session/src/main/java/jwtsession/service/UserServiceProxy.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "users", url = "localhost:8081")
+@FeignClient(name = "users", url = "http://microservice-users.herokuapp.com")
 public interface UserServiceProxy {
 
 	@PostMapping(value="/users/get-first-name")
