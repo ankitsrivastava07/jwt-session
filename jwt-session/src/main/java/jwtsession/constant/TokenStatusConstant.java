70cc7ce457fd303691a4ba96088437a1860817b7
Ankit Srivastava <ankitsrivastava7835@gmail.com>
2021-09-11 20:28:14 +0530

    change

--- jwt-session/src/main/java/jwtsession/constant/TokenStatusConstant.java
+++ jwt-session/src/main/java/jwtsession/constant/TokenStatusConstant.java
@@ -8,7 +8,7 @@ public class TokenStatusConstant {
 	public static final String REFRESH_TOKEN_EXPIRED_DEFAULT_MESSAGE = "Refresh Token expired at ";
 	public static final String TOKEN_VERIFIED_MESSAGE = "Token varified successfully";
 	public static final String TOKEN_EXPIRED = "Your session has been expired.Please logined again";
-	public static final String TOKEN_EXPIRED_TIME = "Your session has been expired at ";
+	public static final String TOKEN_EXPIRED_TIME = "Your session has been expired at  ";
 	public static final String LOGOUT = "Your have successfully logout.";
 	public static final String TOKEN_CREATED = "Your new session has created successfully ";
 	public static final String SERVER_DOWN_DEFAULT_MESSAGE = "Sorry Server is currently down.Please try again later";
